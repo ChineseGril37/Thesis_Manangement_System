be43c47bbbc7d4bab803bf9d6851602a95df90f5
lengs <lengsworkspace@gmail.com>
2024-03-13 09:59:46 +0800

    前后端分离：项目后端创建，使用springboot

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,32 +1,93 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <!-- 头部区域 -->
+    <div style="height: 60px;line-height: 60px;background-color: white;border-radius:5px 5px 5px 5px">
+      <div class="logo" onclick="location.href='http://localhost:8080/'">
+        <img src="@/assets/logo_nobackground.png" alt="" style="width: 60px;height: 60px;position: relative;left: 10px;">
+        <span style="margin-left: 10px;font-size: 25px;float: right;">毕业论文管理系统</span>
+      </div>
+    </div>
+    <!-- 侧边栏和主体 -->
+    <div style="display: flex;margin-top: 5px">
+      <!-- 侧边栏导航 -->
+      <div style="width: 200px;min-height:calc(100vh - 70px);overflow: hidden;margin-right: 5px;background-color: white;border-radius:5px 5px 5px 5px">
+        <el-menu :default-active="$router.path" :default-openeds="['课题申报','论文管理']" router class="el-menu-demo">
+          <el-menu-item index="/">
+            <i class="el-icon-house"></i>
+            <span>首页</span>
+          </el-menu-item>
+          <el-submenu index="课题申报">
+            <template slot="title">
+              <i class="el-icon-folder-opened"></i>
+              <span>课题申报</span>
+            </template>
+            <el-menu-item index="申报课题">
+              <i class="el-icon-document-add"></i>
+              <span>申报课题</span>
+            </el-menu-item>
+            <el-menu-item index="填写开题报告">
+              <i class="el-icon-document"></i>
+              <span>填写开题报告</span>
+            </el-menu-item>
+            <el-menu-item index="填写任务书">
+              <i class="el-icon-document"></i>
+              <span>填写任务书</span>
+            </el-menu-item>
+            <el-menu-item index="填写开题报告">
+              <i class="el-icon-document"></i>
+              <span>填写开题报告</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="论文管理">
+            <template slot="title">
+                <i class="el-icon-folder-opened"></i>
+                <span>论文管理</span>
+              </template>
+            <el-menu-item index="初稿提交">
+              <i class="el-icon-document-add"></i>
+              <span>初稿提交</span>
+            </el-menu-item>
+            <el-menu-item index="初稿修改">
+              <i class="el-icon-document"></i>
+              <span>初稿修改</span>
+            </el-menu-item>
+            <el-menu-item index="格式检测">
+              <i class="el-icon-takeaway-box"></i>
+              <span>格式检测</span>
+            </el-menu-item>
+            <el-menu-item index="定稿提交">
+              <i class="el-icon-document-add"></i>
+              <span>定稿提交</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-more-outline"></i>
+              <span>更多信息</span>
+            </template>
+            <el-menu-item index="/about">
+              <i class="el-icon-info"></i>
+              <span>关于页面</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </div>
+      <!-- 主体数据 -->
+      <div style="flex: 1;min-height:calc(100vh - 70px);background-color: white;border-radius:5px 5px 5px 5px">
+        <router-view/>
+
+      </div>
+    </div>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+import router from "@/router";
+import {defineComponent} from "vue";
+export default defineComponent({
+  methods: {
+    router() {
+      return router
+    }
+  }
+})
+</script>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="padding-left: 20px;margin-top: 20px">
+    <!-- 搜索表单 -->
+    <div style="margin-bottom: 20px">
+      <el-input style="width: 240px" placeholder="请输入姓名"></el-input>
+      <el-input style="width: 240px;margin-left: 10px" placeholder="请输入地址"></el-input>
+      <el-button style="margin-left: 5px" type="primary">
+        <i class="el-icon-search"></i>
+        <span>搜索</span>
+      </el-button>
+    </div>
+    <el-table
+        :data="tableData"
+        style="width: 100%"
+        stripe>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址" width="180"></el-table-column>
+      <el-table-column prop="sexual" label="性别" width="180"></el-table-column>
+      <el-table-column prop="phone" label="联系方式"></el-table-column>
+    </el-table>
+    <div class="block">
+      <el-pagination
+          background
+          :page-size="4"
+          layout="prev, pager, next"
+          :total="20">
+      </el-pagination>
+    </div>
   </div>
 </template>
 
@@ -11,8 +35,14 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      tableData:[
+        { name: '王二', age: 20, address: '北京市', phone: '13276513101', sexual:'男'},
+        { name: '王二', age: 20, address: '北京市', phone: '13276513101', sexual:'男'},
+        { name: '王二', age: 20, address: '北京市', phone: '13276513101', sexual:'男'},
+      ]
+    }
   }
 }
 </script>
